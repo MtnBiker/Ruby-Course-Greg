e1cd8d3cb11f3df37fae892ed8fe1441bd4dd4da
MtnBiker <github@web.knobby.ws>
2014-03-03 13:14:09 -0800

    Finished Week 3 Exercise 7
    RegEx using Thomas p. 68ff

--- w3e7.rb
+++ w3e7.rb
@@ -1,4 +1,4 @@
-# Exercise7. First of all, I'd like to thank Peter Cooper for allowing me to use this exercise.
+# Exercise 7. First of all, I'd like to thank Peter Cooper for allowing me to use this exercise.
 # 
 # The application you're going to develop will be a text analyzer. You will be working on it this and next week. Your Ruby code will read in text supplied in a separate file, analyze it for various patterns and statistics, and print out the results for the user. It's not a 3D graphical adventure or a fancy Web site, but text processing programs are the bread and butter of systems administration and most application development. They can be vital for parsing log files and user-submitted text on Web sites, and manipulating other textual data. With this application you'll be focusing on implementing the features quickly, rather than developing an elaborate object-oriented structure, any documentation, or a testing methodology.
 # 
@@ -30,6 +30,7 @@
 # Split on double newlines to find out how many paragraphs there are.
 # Perform calculations to work out the averages.
 # Create a new, blank Ruby source file and save it as analyzer.rb in your Ruby folder.
+
 text_file_to_process = 'text.txt'
 line_count = 0
 text_complete = ""
@@ -37,18 +38,11 @@ text_complete = ""
 File.open(text_file_to_process, 'r') do |f1|  
   while line = f1.gets  
     lineArr = line.split(" ")
-    # should be done with RegEx, but since we haven't done that yet
-    # lineArr.each { |x|  
-#       i =+ 1
-#       if x == word_to_change
-#         lineArr[i+1] = new_word 
-#       end      
-#     }
     text_complete << lineArr.join(" ") + "\n"
     character_count = text_complete.length
     character_count_minus_whitespace = text_complete.delete(' ').length
     word_count = text_complete.split(" ").length
-    sentence_count = text_complete.split().length
+    sentence_count = text_complete.split(/\.|\?|'!'/).length # careful read of Thomas p68ff
     paragraph_count = text_complete.split("\n\n").length
     words_per_sentence_average = word_count/sentence_count
     sentences_per_paragragh_average = sentence_count/paragraph_count
@@ -56,13 +50,12 @@ File.open(text_file_to_process, 'r') do |f1|
   end  
   puts "\nline_count: #{line_count}"
   puts "\ncharacter_count: #{character_count}"
-  puts "\ncharacter_count_minus_whitespace: #{character_count_minus_whitespace}. Not sure about returns"
-  
+  puts "\ncharacter_count_minus_whitespace: #{character_count_minus_whitespace}. Not sure about returns"  
   puts "\nword_count: #{word_count}"
   puts "\nsentence_count: #{sentence_count}"
-  puts "\nparagraph_count: #{paragraph_count}. NOT WORKING"
-  puts "\nwords_per_sentence_average: #{words_per_sentence_average}.  NOT WORKING BECAUSE PARAGRAPH COUNT ISN'T WORKING"
-  puts "\nsentences_per_paragragh_average: #{sentences_per_paragragh_average}.  NOT WORKING BECAUSE PARAGRAPH COUNT ISN'T WORKING"
+  puts "\nparagraph_count: #{paragraph_count}"
+  puts "\nwords_per_sentence_average: #{words_per_sentence_average}"
+  puts "\nsentences_per_paragragh_average: #{sentences_per_paragragh_average}"
   
 end
 
